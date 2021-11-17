cebfdee347fb4b5f885dfe0e349d37e1ba9b6156
Dmitry Romashov <framebassman@gmail.com>
2021-11-17 09:46:02 +0300

    Add browser into frame

--- src/main/java/tech/romashov/MainFrame.java
+++ src/main/java/tech/romashov/MainFrame.java
@@ -1,15 +1,12 @@
 package tech.romashov;
 
+import com.codebrig.journey.JourneyBrowserView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.romashov.configuration.Environment;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainFrame extends JFrame {
@@ -26,37 +23,16 @@ public class MainFrame extends JFrame {
 
     private void initFrame() {
         this.setName("MainFrame");
-        int x = ThreadLocalRandom.current().nextInt(0, 300 + 1);
-        int y = ThreadLocalRandom.current().nextInt(0, 300 + 1);
-        this.setLocation(x, y);
+        this.setLocation(
+                ThreadLocalRandom.current().nextInt(0, 300 + 1),
+                ThreadLocalRandom.current().nextInt(0, 300 + 1)
+        );
         if (environment == Environment.Development || environment == Environment.Production) {
             setDefaultCloseOperation(EXIT_ON_CLOSE);
         }
-
-        // Создание панели с двумя кнопками
-        JPanel contents = new JPanel();
-        JLabel logLabel = new JLabel();
-        JButton button = new JButton("Button");
-        button.setName("TestButton");
-        button.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                logLabel.setText("Button was clicked");
-                logLabel.setName("LogLabel");
-            }
-        });
-
-        contents.add(button);
-        contents.add(logLabel);
-
-        // Замена панели содержимого
-        setContentPane(contents);
-
-        // Определение размера окна
-        setSize(200, 100);
-        // Открытие окна
+        JourneyBrowserView browser = new JourneyBrowserView("https://google.com");
+        getContentPane().add(browser, BorderLayout.CENTER);
         setVisible(true);
-        setAlwaysOnTop(true);
+        setSize(200, 100);
     }
 }
